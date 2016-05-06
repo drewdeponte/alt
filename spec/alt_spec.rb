ab327fe64f1e242eadfe32ad104e453f42654965
Andrew De Ponte <cyphactor@gmail.com>
2016-05-06 09:58:18 -0700

    Converted alt specs to integration tests
    Why you made the change:
    
    I did this because the layer between the two isn't much more costly and provides
    more flexibility from a testing standpoint.

--- spec/alt_spec.rb
+++ spec/alt_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe Alt do
+  context 'when given a path and set of possible alternate paths as stdin' do
+    it 'displays the most similar alternate path from the possibles' do
+      output = `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./alt -- spec/models/topic_link_click_spec.rb`
+      expect(output).to eq('app/models/topic_link_click.rb')
+    end
+  end
+
   context 'when given the -v option' do
     it 'displays the version' do
-      expect { Alt.new(argv: ["-v"]).main }.to output("alt v#{Alt::VERSION}\n").to_stdout
+      output = `./alt -v`
+      expect(output).to eq("alt v#{Alt::VERSION}\n")
     end
   end
 
   context 'when given the --version option' do
     it 'displays the version' do
-      expect { Alt.new(argv: ["--version"]).main }.to output("alt v#{Alt::VERSION}\n").to_stdout
+      output = `./alt --version`
+      expect(output).to eq("alt v#{Alt::VERSION}\n")
     end
   end
 end
