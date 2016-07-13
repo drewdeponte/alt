9d0cf55259f446324cbd2c809bc165b402e8929b
Andrew De Ponte <cyphactor@gmail.com>
2016-07-13 00:56:01 -0700

    Make argument parsing work
    Why you made the change:
    
    I did this so that we could pretty much match the existing command line options
    from the Ruby version. I strayed from being 100% same because I reviewed cat,
    and tar commands and they use a -f FILE switch and specify FILE as a single dash
    to read from stdin. Given that model and the fact that I was having difficulties
    getting this argument parser to do what we are currently doing in Ruby, I opted
    following cat, nad tar's pattern.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, StoreTrue, Store, StoreOption, Print};
+
 fn main() {
-  println!("Hello, world!");
+    let mut debug = false;
+    let mut path = "".to_string();
+    let mut file: Option<String> = None;
+    { // block limits of borrows by refer() method calls
+        let mut ap = ArgumentParser::new();
+        ap.add_option(&["-v", "--version"], Print(env!("CARGO_PKG_VERSION").to_string()), "show version");
+        ap.refer(&mut debug).add_option(&["-d", "--debug"], StoreTrue, "enable debug output");
+        ap.refer(&mut file).add_option(&["-f", "--file"], StoreOption, "possible alternates file, - for stdin");
+        ap.refer(&mut path).add_argument("PATH", Store, "path to find alternate for").required();
+        ap.parse_args_or_exit();
+    }
+
+    println!("required path: {}", path);
+
+    if let Some(unwrapped_file) = file {
+        println!("file: {}", unwrapped_file);
+    }
+
+    println!("debug: {}", debug);
 }
