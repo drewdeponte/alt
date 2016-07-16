d816d90b4aacff3049b63254996248fa7d34f1a4
Andrew De Ponte <cyphactor@gmail.com>
2016-07-15 23:46:51 -0700

    Get glob working with possible paths
    Why you made the change:
    
    I did this so that if the user didn't specify an input file it would be able to
    fetch the possible paths from a glob.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 extern crate argparse;
+extern crate glob;
 
 use argparse::{ArgumentParser, StoreTrue, Store, StoreOption, Print};
 use std::io::BufRead;
+use glob::glob;
+
+fn identity<T>(x: T) -> T {x}
 
 struct Options {
     debug: bool,
     path: String,
     file: Option<String>
+}
+
+fn get_possible_files_from_glob() -> Result<Vec<std::path::PathBuf>, glob::PatternError> {
+    // Dir.glob("**/*").reject { |p| File.directory?(p) }.map { |p| Path.new(p) }
+    return glob("**/*").map(|paths| paths.flat_map(identity).filter(|path| path.is_file()).collect())
 }
 
 fn main() {
@@ -35,7 +44,16 @@ fn main() {
               println!("{}", line.unwrap());
             }
         } else {
-          println!("Figure out how open and read actual file");
+            // figure out how to open specified file and read in as content
+        }
+    } else {
+        match get_possible_files_from_glob() {
+            Ok(paths) => {
+                for path in paths.iter() {
+                    println!("{:?}", path.display());
+                }
+            },
+            Err(e) => println!("{:?}", e)
         }
     }
 
