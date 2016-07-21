7c0e1382bea832cc0c3ab337ed032f3554553bdd
Andrew De Ponte <cyphactor@gmail.com>
2016-07-20 22:13:42 -0700

    Remove the ruby scatter gather implementation
    Why you made the change:
    
    I did this because I created it just to benchmark it against the classic and the
    results show that it isn't worth the overhead of creating the threads and doing
    a scatter gather in Ruby. Also, the Rust version is significantly more
    performant than the Ruby version so we will likely be moving away from the Ruby
    version officially.

--- benchmark.rb
+++ benchmark.rb
@@ -15,18 +15,6 @@ Benchmark.bm(15) do |x|
   end
 end
 
-puts "\nRuby Scatter Gather Implementation"
-
-Benchmark.bm(15) do |x|
-  x.report('For impl. file:') do
-    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./alt_sg -- lib/rubyfile_qq.rb` }
-  end
-
-  x.report('For test file:') do
-    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./alt_sg -- spec/lib/rubyfile_qr_spec.rb` }
-  end
-end
-
 puts "\nRust Classic Implementation"
 
 Benchmark.bm(15) do |x|
@@ -48,18 +36,6 @@ Benchmark.bm(15) do |x|
 
   x.report('For test file:') do
     repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./alt -- spec/controllers/admin/groups_controller.rb` }
-  end
-end
-
-puts "\nRuby Scatter Gather Implementation - Discourse"
-
-Benchmark.bm(15) do |x|
-  x.report('For impl. file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./alt_sg -- app/controllers/admin/groups_controller.rb` }
-  end
-
-  x.report('For test file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./alt_sg -- spec/controllers/admin/groups_controller.rb` }
   end
 end
 
