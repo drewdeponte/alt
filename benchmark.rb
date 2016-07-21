ebe66b138c9d60b053e7cc2d5b65d4ec73bdd596
Andrew De Ponte <cyphactor@gmail.com>
2016-07-21 00:23:03 -0700

    Remove the ruby version from the benchmarks
    Why you made the change:
    
    I did this because the ruby version no longer exists and therefore was causing
    the benchmarks script to fail.

--- benchmark.rb
+++ benchmark.rb
@@ -1,19 +1,6 @@
 require 'benchmark'
 
 repeat = 1_000
-# repeat = 1
-
-puts "\nRuby Classic Implementation"
-
-Benchmark.bm(15) do |x|
-  x.report('For impl. file:') do
-    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./alt -- lib/rubyfile_qq.rb` }
-  end
-
-  x.report('For test file:') do
-    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./alt -- spec/lib/rubyfile_qr_spec.rb` }
-  end
-end
 
 puts "\nRust Classic Implementation"
 
@@ -24,18 +11,6 @@ Benchmark.bm(15) do |x|
 
   x.report('For test file:') do
     repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./target/release/alt -- spec/lib/rubyfile_qr_spec.rb` }
-  end
-end
-
-puts "\nRuby Classic Implementation - Discourse"
-
-Benchmark.bm(15) do |x|
-  x.report('For impl. file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./alt -- app/controllers/admin/groups_controller.rb` }
-  end
-
-  x.report('For test file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./alt -- spec/controllers/admin/groups_controller.rb` }
   end
 end
 
