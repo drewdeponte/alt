7f2d64f79e8b28e983ee619d0e3c8768abc4b317
Nico Ritschel <nico@Nicos-MacBook-Pro.local>
2017-08-07 21:11:14 -0700

    Add ScalaTest

--- src/alt/path/alteration/mod.rs
+++ src/alt/path/alteration/mod.rs
@@ -3,7 +3,7 @@ extern crate regex;
 use self::regex::Regex;
 
 pub fn strip_test_words(filename: &String) -> String {
-    let re = Regex::new(r"(test_)?(?P<p>\w+?)(_rake_spec|_spec|_rake_test|_test|_steps|Tests|UITests|Specs|UISpecs|Test|Spec)?(\.\w+)?$").unwrap();
+    let re = Regex::new(r"(test_)?(?P<p>\w+?)(_rake_spec|_spec|_rake_test|_test|_steps|Tests|UITests|Specs|UISpecs|Test|Spec|Suite)?(\.\w+)?$").unwrap();
     re.replace_all(filename.as_str(), "$p")
 }
 
@@ -116,6 +116,26 @@ mod tests {
     #[test]
     fn strip_test_words_returns_filename_with_junit_test_words_stripped() {
         let s = String::from("SomethingTest.java");
+        assert_eq!(strip_test_words(&s), "Something");
+    }
+
+    // Scala ScalaTest
+
+    #[test]
+    fn strip_test_words_returns_filename_with_scalatest_test_word_stripped() {
+        let s = String::from("SomethingTest.scala");
+        assert_eq!(strip_test_words(&s), "Something");
+    }
+
+    #[test]
+    fn strip_test_words_returns_filename_with_scalatest_spec_word_stripped() {
+        let s = String::from("SomethingSpec.scala");
+        assert_eq!(strip_test_words(&s), "Something");
+    }
+
+    #[test]
+    fn strip_test_words_returns_filename_with_scalatest_suite_word_stripped() {
+        let s = String::from("SomethingSuite.scala");
         assert_eq!(strip_test_words(&s), "Something");
     }
 }
