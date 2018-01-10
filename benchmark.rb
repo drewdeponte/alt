ac9b66722dade39a0c9b0c9b54e9f758f38c3d46
Andrew De Ponte <cyphactor@gmail.com>
2018-01-10 08:14:43 -0800

    Add full blown scenarios to benchmark.rb script
    I did this so that we would not only get benchmarks on the scenario
    where alt is handed the possibles but also the full blown scenarios
    where someone uses find combined with alt and the scenario where someone
    uses the Glob/WalkDir path with alt. That way we could see the
    performance between those two use cases as they are the primary use
    cases.

--- benchmark.rb
+++ benchmark.rb
@@ -2,7 +2,7 @@ require 'benchmark'
 
 repeat = 1_000
 
-puts "\nRust Classic Implementation"
+puts "\n/usr/local/bin/alt - aa_zz_possibiles.txt fixture"
 
 Benchmark.bm(15) do |x|
   x.report('For impl. file:') do
@@ -14,19 +14,7 @@ Benchmark.bm(15) do |x|
   end
 end
 
-puts "\nRust Classic Implementation - Discourse"
-
-Benchmark.bm(15) do |x|
-  x.report('For impl. file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | /usr/local/bin/alt -f - app/controllers/admin/groups_controller.rb` }
-  end
-
-  x.report('For test file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | /usr/local/bin/alt -f - spec/controllers/admin/groups_controller.rb` }
-  end
-end
-
-puts "\nRust Latest Build Implementation"
+puts "\ntarget/release/alt - aa_zz_possibles.txt fixture"
 
 Benchmark.bm(15) do |x|
   x.report('For impl. file:') do
@@ -38,7 +26,19 @@ Benchmark.bm(15) do |x|
   end
 end
 
-puts "\nRust Latest Build Implementation - Discourse"
+puts "\n/usr/local/bin/alt - ruby_on_rails_discourse_possibles.txt fixture"
+
+Benchmark.bm(15) do |x|
+  x.report('For impl. file:') do
+    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | /usr/local/bin/alt -f - app/controllers/admin/groups_controller.rb` }
+  end
+
+  x.report('For test file:') do
+    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | /usr/local/bin/alt -f - spec/controllers/admin/groups_controller.rb` }
+  end
+end
+
+puts "\ntarget/release/alt - ruby_on_rails_discourse_possibles.txt fixture"
 
 Benchmark.bm(15) do |x|
   x.report('For impl. file:') do
@@ -49,3 +49,55 @@ Benchmark.bm(15) do |x|
     repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./target/release/alt -f - spec/controllers/admin/groups_controller.rb` }
   end
 end
+
+Dir.chdir('spec/fixtures/discourse')
+
+puts "\n/usr/local/bin/alt - With cd spec/fixtures/discourse using Glob/WalkDir"
+
+Benchmark.bm(15) do |x|
+  x.report('For impl. file:') do
+    repeat.times { `/usr/local/bin/alt app/controllers/admin/groups_controller.rb` }
+  end
+
+  x.report('For test file:') do
+    repeat.times { `/usr/local/bin/alt spec/controllers/admin/groups_controller.rb` }
+  end
+end
+
+puts "\ntarget/release/alt - With cd spec/fixtures/discourse using Glob/WalkDir"
+
+Benchmark.bm(15) do |x|
+  x.report('For impl. file:') do
+    repeat.times { `../../../target/release/alt app/controllers/admin/groups_controller.rb` }
+  end
+
+  x.report('For test file:') do
+    repeat.times { `../../../target/release/alt spec/controllers/admin/groups_controller.rb` }
+  end
+end
+
+puts "\n/usr/local/bin/alt - With cd spec/fixtures/discourse using find"
+
+Benchmark.bm(15) do |x|
+  x.report('For impl. file:') do
+    repeat.times { `find . -not -path '*/\.*' -type f | /usr/local/bin/alt -f - app/controllers/admin/groups_controller.rb` }
+  end
+
+  x.report('For test file:') do
+    repeat.times { `find . -not -path '*/\.*' -type f  | /usr/local/bin/alt -f - spec/controllers/admin/groups_controller.rb` }
+  end
+end
+
+puts "\ntarget/release/alt - With cd spec/fixtures/discourse using find"
+
+Benchmark.bm(15) do |x|
+  x.report('For impl. file:') do
+    repeat.times { `find . -not -path '*/\.*' -type f | ../../../target/release/alt -f - app/controllers/admin/groups_controller.rb` }
+  end
+
+  x.report('For test file:') do
+    repeat.times { `find . -not -path '*/\.*' -type f  | ../../../target/release/alt -f - spec/controllers/admin/groups_controller.rb` }
+  end
+end
+
+Dir.chdir('../../../')
