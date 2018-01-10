92a23865ab60d4898bd18ad58698963f6db43d2f
Andrew De Ponte <cyphactor@gmail.com>
2018-01-09 16:56:02 -0800

    Changed args to proper args & reference installed
    I did this so that it will behave correctly in terms since it is now
    getting the correct command line arguments in the benchmarks.
    
    I also set it up so that one of the sets references the "Rust Classic"
    or rather the version currently installed at /usr/local/bin/alt and the
    "Latest Build" version as the one that is relatively location at
    ./target/release/alt. This makes it easier to benchmark the changes you
    just made against the latest stable release in most cases.

--- benchmark.rb
+++ benchmark.rb
@@ -6,11 +6,11 @@ puts "\nRust Classic Implementation"
 
 Benchmark.bm(15) do |x|
   x.report('For impl. file:') do
-    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./target/release/alt -- lib/rubyfile_qq.rb` }
+    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | /usr/local/bin/alt -f - lib/rubyfile_qq.rb` }
   end
 
   x.report('For test file:') do
-    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./target/release/alt -- spec/lib/rubyfile_qr_spec.rb` }
+    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | /usr/local/bin/alt -f - spec/lib/rubyfile_qr_spec.rb` }
   end
 end
 
@@ -18,11 +18,34 @@ puts "\nRust Classic Implementation - Discourse"
 
 Benchmark.bm(15) do |x|
   x.report('For impl. file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./target/release/alt -- app/controllers/admin/groups_controller.rb` }
+    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | /usr/local/bin/alt -f - app/controllers/admin/groups_controller.rb` }
   end
 
   x.report('For test file:') do
-    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./target/release/alt -- spec/controllers/admin/groups_controller.rb` }
+    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | /usr/local/bin/alt -f - spec/controllers/admin/groups_controller.rb` }
   end
 end
 
+puts "\nRust Latest Build Implementation"
+
+Benchmark.bm(15) do |x|
+  x.report('For impl. file:') do
+    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./target/release/alt -f - lib/rubyfile_qq.rb` }
+  end
+
+  x.report('For test file:') do
+    repeat.times { `cat spec/fixtures/aa_zz_possibles.txt | ./target/release/alt -f - spec/lib/rubyfile_qr_spec.rb` }
+  end
+end
+
+puts "\nRust Latest Build Implementation - Discourse"
+
+Benchmark.bm(15) do |x|
+  x.report('For impl. file:') do
+    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./target/release/alt -f - app/controllers/admin/groups_controller.rb` }
+  end
+
+  x.report('For test file:') do
+    repeat.times { `cat spec/fixtures/ruby_on_rails_discourse_possibles.txt | ./target/release/alt -f - spec/controllers/admin/groups_controller.rb` }
+  end
+end
