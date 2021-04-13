0647f9dc90230b2bcef032a556715a73a1672eb1
Tabitha <tabitha@keyneston.com>
2021-04-13 14:29:09 +0200

    Add support for foo_test.go file types

--- src/alt/path/alteration/mod.rs
+++ src/alt/path/alteration/mod.rs
@@ -6,7 +6,10 @@ pub fn strip_test_words(filename: &str) -> &str {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"(test_)?(?P<p>\w+?)(_rake_spec|_spec|_rake_test|_test|_steps|.test|.spec|Tests|UITests|Specs|UISpecs|Test|Spec|Suite)?(\.\w+)?$").unwrap();
     }
-    RE.captures(filename).and_then(|caps| caps.name("p")).map(|m| m.as_str()).unwrap_or(filename)
+    RE.captures(filename)
+        .and_then(|caps| caps.name("p"))
+        .map(|m| m.as_str())
+        .unwrap_or(filename)
 }
 
 #[cfg(test)]
@@ -161,5 +164,12 @@ mod tests {
     fn strip_test_words_returns_filename_with_scalatest_suite_word_stripped() {
         let s = String::from("SomethingSuite.scala");
         assert_eq!(strip_test_words(&s), "Something");
+    }
+
+    // Go
+    #[test]
+    fn strip_test_words_returns_filename_with_go_test_words_stripped() {
+        let s = String::from("foo_test.go");
+        assert_eq!(strip_test_words(&s), "foo");
     }
 }
